5b8c17b2e5c012d05b61bf42efd69b6d77a1c5c3
John B. Buncher <jbuncher@gmail.com>
2017-06-26 17:00:28 -0500

    fixed key import (and key file) to have correct formatting (lots of columns, not lots of rows)
    separated the import filename out
    set up an export filename

--- PhysPortConverterFCI.R
+++ PhysPortConverterFCI.R
@@ -7,27 +7,28 @@
 # not the case!
 
 # Imports the data file
-ImportToPhysPort <- read.csv("~/Teaching/NDSU/Phys 211/2015 Spring/FCI PrePost Test/ImportToPhysPort_Post_2015.csv", stringsAsFactors = FALSE)
+filename = "~/Teaching/NDSU/Phys 211/2015 Spring/FCI PrePost Test/ImportToPhysPort_Post_2015.csv"
+ImportToPhysPort <- read.csv(filename, stringsAsFactors = FALSE)
 
 # Reads in the FCI key.  This file will NOT be made public!  If you need
 # access to it, please go through the proper channels
-read.csv("FCIkey.csv")
+FCIkey <- read.csv("FCIkey.csv", stringsAsFactors = FALSE)
 
 # function to fix the responses.  This takes in a matrix (our imported csv)
 # and goes through each of the question columns, replacing all "$" with 
 # the actual response ("A", "B",..., or "E").
 
-fix.responses <- function(matrix,keyfile) {
-  for (i in 1:length(keyfile)) matrix[i+4][matrix[i+4] == "$"] <- keyfile[i]
+fix.responses <- function(matrix, key) {
+  for (i in 1:length(key)) matrix[i+4][matrix[i+4] == "$"] <- key[i]
 return (matrix)}
 
 # This was some "lapply" stuff I was trying rather than using the loop.
 # Clearly, I didn't get far
 # lapply(unique_vals, function(elem) elem[2])
 
+exportfilename = paste0("testname","_","fixed",".csv")
 # Copies the fixed table to another variable
-FixedPostTable <- ImportToPhysPort
+FixedPostTable <- fix.responses(ImportToPhysPort, FCIkey)
 
 # Writes the fixed table out to a csv file
-write.csv(FixedPostTable, file = "FixedPostTable.csv")
-
+write.csv(FixedPostTable, file = exportfilename)
